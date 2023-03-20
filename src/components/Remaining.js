4b3a1b07e9cb2ad23830bf59779a3bb91bf23547
yossiKrauze <joseph.krauze@gmail.com>
2023-03-19 23:14:00 -0400

    simplified the Remaining component by re-using Remaining from the AppContext

--- src/components/Remaining.js
+++ src/components/Remaining.js
@@ -2,19 +2,19 @@ import React, { useContext } from 'react';
 import { AppContext } from '../context/AppContext';
 
 const Remaining = () => {
-    const { expenses, budget } = useContext(AppContext);
+    const { expenses, budget, totalExpenses, remaining } = useContext(AppContext);
 
-    const initialTotalValue = 0;
-    const totalExpenses = expenses.reduce(
-        (total, item) => {
-            return (total = total + item.cost);
-        }, initialTotalValue); //The reduce function's second parameter is the initial total value. The function then calculate the total ACCUMULATIVE sum
+    // const initialTotalValue = 0;
+    // const totalExpenses = expenses.reduce(
+    //     (total, item) => {
+    //         return (total = total + item.cost);
+    //     }, initialTotalValue); //The reduce function's second parameter is the initial total value. The function then calculate the total ACCUMULATIVE sum
 
     const alertType = totalExpenses >= budget ? 'alert-danger' : 'alert-success';
 
     return (
         <div className={`alert ${alertType}`}>
-            <span>Remaining: ${budget - totalExpenses}</span>    
+            <span>Remaining: ${remaining}</span>    
         </div>
     )
 };
