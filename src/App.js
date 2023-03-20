b56ff65a9adbe04cc14f82ce3ea3c49c23999857
yossiKrauze <joseph.krauze@gmail.com>
2023-03-20 00:38:34 -0400

    finished the exercise

--- src/App.js
+++ src/App.js
@@ -5,8 +5,11 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Budget from './components/Budget';
 import Remaining from './components/Remaining';
 import ExpenseTotal from './components/ExpenseTotal';
+import ExpenseList from './components/ExpenseList';
 
 import { AppProvider } from './context/AppContext';
+import ExpenseItem from './components/ExpenseItem';
+import AllocationForm from './components/AllocationForm';
 
 const App = () => {
     return (
@@ -14,30 +17,28 @@ const App = () => {
             <div className='container'>
                 <h1 className='mt-3'>Company's Budget Allocation</h1>
                 <div className='row mt-3'>
-                    {
-                        <div className='col-sm'>
-                            <Budget />
-                        </div>
-                    }
+                    <div className='col-sm'>
+                        <Budget />
+                    </div>
+                    <div className='col-sm'>
+                        <Remaining />
+                    </div>
+                    <div className='col-sm'>
+                        <ExpenseTotal />
+                    </div>
 
-                    {
-                        <div className='col-sm'>
-                            <Remaining />
-                        </div>
-                    }
-
-                    {
-                        <div className='col-sm'>
-                            <ExpenseTotal />
-                        </div>
-                    }
-
-                    {/* Add ExpenseList component here under */}
-
-                    {/* Add ExpenseItem component here under */}
-
-                    {/* Add AllocationForm component here under */}
-
+                </div>
+                <h3 className='mt-3'>Allocation</h3>
+                <div className='row '>
+                    <div className='col-sm'>
+                        <ExpenseList />
+                    </div>
+                </div>
+                <h3 className='mt-3'>Change Allocation</h3>
+                <div className='row mt-3'>
+                    <div className='col-sm'>
+                        <AllocationForm />
+                    </div>
                 </div>
             </div>
         </AppProvider>
